c22b70bedf9c6b1778ce1c6970ca33b68461a092
Riley <dakimi07@gmail.com>
2024-12-08 22:22:04 +0800

    update Q3-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -158,6 +158,11 @@ where user_id = (select id from "USER" where email = 'starplatinum@hexschooltest
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
 
+insert into "SKILL" (name)
+values ('空中瑜伽');
+
+delete from "SKILL"
+where name = '空中瑜伽';
 
 --  ████████  █████   █    █   █ 
 --    █ █   ██    █  █     █   █ 
