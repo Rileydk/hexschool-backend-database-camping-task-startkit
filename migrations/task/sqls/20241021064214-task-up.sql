2a512b1ce988087fc11d65c1015dc298d7bfa6be
Riley <dakimi07@gmail.com>
2024-12-09 00:10:05 +0800

    update Q6-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -331,9 +331,12 @@ limit 1;
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
 
 select
-	sum(purchased_credits)
+	"CREDIT_PACKAGE".name as 組合包方案名稱,
+	count(*) as 銷售數量
 from "CREDIT_PURCHASE"
-where created_at between '2024-11-01 00:00:00' and '2024-11-30 23:59:59';
+join "CREDIT_PACKAGE" on "CREDIT_PURCHASE".credit_package_id = "CREDIT_PACKAGE".id
+where "CREDIT_PURCHASE".created_at between '2024-12-01 00:00:00' and '2024-12-30 23:59:59'
+group by "CREDIT_PACKAGE".name;
 
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
